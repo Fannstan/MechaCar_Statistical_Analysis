62a9bbf122d51769a7bc724228ad7d8c79de5d20
Fannstan <fredrica.stanley@gmail.com>
2022-08-07 15:35:08 -0400

    updating with deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,19 @@ total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI
 ### Lot Summary ###
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+#### Deliverable 3 ####
+
+### t test Across All Manufacturing Lots ###
+
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+### t test For Manufacturing Lot 1 ###
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == 'Lot1'),mu=mean(Suspension_Coil$PSI))
+
+### t test For Manufacturing Lot 2 ### 
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == 'Lot2'),mu=mean(Suspension_Coil$PSI))
+
+### t test For Manufacturing Lot 3 ###
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == 'Lot3'),mu=mean(Suspension_Coil$PSI))
